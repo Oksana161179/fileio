afa57d17860e21f6222cd535b294b73bc5bc0b8b
Оксана <oksana.fomenko.161979@gmail.com>
2024-10-30 20:14:44 +0600

    Добавлена обработка исключений

--- fileio.py
+++ fileio.py
@@ -1,19 +1,27 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from tkinter import ttk
 import requests#запросы к файлу, которые мы будем делать
 
 
 def upload():#создаем функцию загрузки файлов
-    filepath = fd.askopenfilename()#получаем путь к файлу-filepath, который будем загружать
-    if filepath:#проверяем, если переменная-filepath не пустая
-        files = {'file': open(filepath, 'rb')}#открываем файл в режиме чтения байтов-rb
-        response = requests.post('https://file.io', files=files)#получаем ответ-response,
-        # отправляя запрос-requests
-        if response.status_code == 200:#делаем проверку, если ответ-200, то все хорошо
-            link = response.json()['link']#и мы можем в ссылку положить то,
-            # что нам прислали в ответ. указав ключ-['link']
-            entry.insert(0, link)#выводим в поле ввода, с начальной позицией ноль и вставляем ссылку-link
+    try:#попробовать
+        filepath = fd.askopenfilename()#получаем путь к файлу-filepath, который будем загружать
+        if filepath:#проверяем, если переменная-filepath не пустая
+            with open(filepath, 'rb') as f:#открываем файл в режиме чтения байтов-rb
+                files = {'file': f}#открываем файл в режиме чтения байтов-rb
+                response = requests.post('https://file.io', files=files)#получаем ответ-response,
+                # отправляя запрос-requests
+                response.raise_for_status()#строчка позволяет проверить не было ли ошибок
+                link = response.json()['link']#и мы можем в ссылку положить то,
+                # что нам прислали в ответ. указав ключ-['link']
+                entry.delete(0, END)#очищаем поле ввода,
+                # перед тем как вставить ссылку, от начала и до конца
+                entry.insert(0, link)#выводим в поле ввода,
+                # с начальной позицией ноль и вставляем ссылку-link
+    except Exception as e:#обрабатываем исключения
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")#создаем окно, которое оповестит об ошибке
 
 
 window = Tk()#создаем окно
